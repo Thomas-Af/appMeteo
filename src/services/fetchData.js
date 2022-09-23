57d8e6dda8e602b60d25521e67f960f4e229f9bb
Thomas-Af <thomas.afonso@mail-ecv.fr>
2022-09-23 17:53:48 +0200

    change token

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -1,6 +1,5 @@
 const url = 'https://api.meteo-concept.com/api/'
 const REACT_APP_TOKEN_API = '95ac05afb0a6914eabe53c25723f147585bc526b0522ff7d808b0208cd61f664'
-const REACT_APP_TOKEN_API_2 = 'f13320afe3014096ad611827faba2c277ef497f76e0389288b7d9d93e22e376d'
 
 
 
